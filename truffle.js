9496eba54a1e554a76fb10a2fe0eafc98768cbd9
John Barker <jebarker@gmail.com>
2019-01-07 15:37:13 -0700

    Use same port as is typically used in chainlink project

--- truffle.js
+++ truffle.js
@@ -3,7 +3,7 @@ module.exports = {
     networks: {
         development: {
             host: '127.0.0.1',
-            port: 8545,
+            port: 18545,
             network_id: '*' // Match any network id
         },
         ganache_cli: {
